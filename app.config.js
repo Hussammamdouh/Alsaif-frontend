a3a2d3ab45461c37dbfaebd5bf235f8c19e65aec
Hussammamdouh <125016995+Hussammamdouh@users.noreply.github.com>
2026-08-02 22:04:46 +0300

    pumped the app version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Alsaif Analysis",
     slug: "elsaif-analysis",
-    version: "1.0.7",
+    version: "1.0.8",
     orientation: "portrait",
     icon: "./assets/Logo Secondry.jpg",
     userInterfaceStyle: "automatic",
@@ -44,7 +44,7 @@ export default {
       }
     },
     android: {
-      versionCode: 35,
+      versionCode: 36,
       adaptiveIcon: {
         foregroundImage: "./assets/Logo Secondry.jpg",
         backgroundColor: "#ffffff"
